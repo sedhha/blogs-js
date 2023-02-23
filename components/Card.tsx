e2084039430f88d767e152426fbafa08c4829291
sedhha <activity.schoolsh2@gmail.com>
2023-02-23 20:33:37 +0530

    Updated to respond to whole link

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,33 +8,39 @@ const Card = ({ imageUrl, date, title, excerpt, cta, creator }: ICardProps) => {
 		year: 'numeric',
 	});
 	return (
-		<div className='card'>
-			<div className='card-img'>
-				<img
-					src={imageUrl ?? '/image.webp'}
-					alt='Image'
-				/>
-			</div>
-			<div className='card-body'>
-				<div className='card-date'>{formattedDate ?? 'February 23, 2023'}</div>
-				<h3
-					className='card-title'
-					// Setting inner HTML purposely. However passing script tags could cause problems using it as running out of time now
-					dangerouslySetInnerHTML={{ __html: title }}
-				></h3>
-				<div
-					className='card-excerpt'
-					// Setting inner HTML purposely. However passing script tags could cause problems using it as running out of time now
-					dangerouslySetInnerHTML={{ __html: excerpt }}
-				></div>
-				<div className='card-link'>
-					<Link href={`posts/${cta ?? '#'}`}>
-						<p>Read more</p>
-					</Link>
+		<Link href={`posts/${cta ?? '#'}`}>
+			<div className='card'>
+				<div className='card-img'>
+					<img
+						src={imageUrl ?? '/image.webp'}
+						alt='Image'
+					/>
 				</div>
-				<div className='card-creator'>Created by: {creator ?? 'John Doe'}</div>
+				<div className='card-body'>
+					<div className='card-date'>
+						{formattedDate ?? 'February 23, 2023'}
+					</div>
+					<h3
+						className='card-title'
+						// Setting inner HTML purposely. However passing script tags could cause problems using it as running out of time now
+						dangerouslySetInnerHTML={{ __html: title }}
+					></h3>
+					<div
+						className='card-excerpt'
+						// Setting inner HTML purposely. However passing script tags could cause problems using it as running out of time now
+						dangerouslySetInnerHTML={{ __html: excerpt }}
+					></div>
+					<div className='card-link'>
+						<Link href={`posts/${cta ?? '#'}`}>
+							<p>Read more</p>
+						</Link>
+					</div>
+					<div className='card-creator'>
+						Created by: {creator ?? 'John Doe'}
+					</div>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
